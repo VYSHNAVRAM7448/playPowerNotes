dfdfccf9ff116c950a7b3204c2034f256731109c
VYSHNAVRAM <vyshnavk7448@gmail.com>
2024-12-24 18:24:00 +0530

    Dist only for Deployment

---
+++ dist/play-power-notes/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-I4A7Z4B4.mjs";import"./chunk-JPTRBLQM.mjs";import"./chunk-LSLGP5BZ.mjs";import"./chunk-FD53EANF.mjs";import"./chunk-EBITJBPR.mjs";import"./chunk-PWKT3LZT.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/app/app.routes.ts
+++
@@ -1,4 +0,0 @@
-import { Routes } from '@angular/router';
-import { MainLayoutComponent } from './main-layout/main-layout.component';
-
-export const routes: Routes = [{ path: '', component: MainLayoutComponent }];
